341db6b3f1ebc980ed741ef86d3dcbb0683babb8
Omkar-IshanBookLife <92090624+Omkar-IshanBookLife@users.noreply.github.com>
2021-10-08 16:51:38 +0530

    Add files via upload

---
+++ knowmore.js
@@ -0,0 +1,10 @@
+var knowOmkar = document.getElementById("knowOmkar");
+var knowIshan = document.getElementById("knowIshan");
+knowOmkar.onclick = function (){
+    window.open("mailto:ogamethorns@gmail.com?subject=know more about omkar");
+    console.log("Omkar");
+}
+knowIshan.onclick = function (){
+    window.open("mailto:maliishan965@gmail.com?subject=know more about ishan");
+    console.log("Omkar"); 
+}
